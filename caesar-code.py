9631bfb8ec4f87a7a0bdc37b4b5f59310965ab0d
Terrainaheadpullup <52794963+Terrainaheadpullup@users.noreply.github.com>
2021-06-22 18:48:30 +0100

    Update caesar-code.py

--- caesar-code.py
+++ caesar-code.py
@@ -5,7 +5,7 @@ letter_number_dict = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H
 
 # for number to letter conversions
 number_letter_dict = {1: "A", 2: "B", 3: "C", 4: "D", 5: "E", 6: "F", 7: "G", 8: "H", 9: "I", 10: "J", 11: "K",
-                      12: "L", 13: "M", 14: "M", 15: "O", 16: "P", 17: "Q", 18: "R", 19: "S", 20: "T", 21: "U",
+                      12: "L", 13: "M", 14: "N", 15: "O", 16: "P", 17: "Q", 18: "R", 19: "S", 20: "T", 21: "U",
                       22: "V", 23: "W", 24: "X", 25: "Y", 26: "Z", " ": " "}
 
 # enter text in block capitals
